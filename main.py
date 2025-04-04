55e3adcd2e2fb402bb5f5527781c177545c0ae41
Erin Hamalainen <erinvh620@gmail.com>
2025-04-04 16:29:36 -0700

    Update main.py to use QuoteService

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from Quotes.QuoteService import QuoteService
 from Quotes.AlpacaWrapper import AlpacaWrapper
 
-print(AlpacaWrapper.get_quote(self=AlpacaWrapper,symbol="AAPL"))
+
+# print(QuoteService.fetch_quote(self=QuoteService, symbol="AAPL"))
+
+alpaca_api = AlpacaWrapper()
+quote_service = QuoteService(quote_api=alpaca_api)
+
+quote = quote_service.fetch_quote("AAPL")
+print(quote)
