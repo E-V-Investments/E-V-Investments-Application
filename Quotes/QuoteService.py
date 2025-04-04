4b39919c83c0a85cb63d2232d6c7d2445c5c5403
Erin Hamalainen <erinvh620@gmail.com>
2025-04-04 16:29:09 -0700

    Created a QuoteService

---
+++ Quotes/QuoteService.py
@@ -0,0 +1,10 @@
+from Quotes.QuoteAPIProtocol import QuoteAPIProtocol
+from models.quote import Quote
+
+
+class QuoteService:
+    def __init__(self, quote_api: QuoteAPIProtocol):
+        self.quote_api = quote_api
+
+    def fetch_quote(self, symbol: str) -> Quote:
+        return self.quote_api.fetch_quote(symbol)
