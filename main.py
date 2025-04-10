4ed362a7526864b8b64bf98c7227c31f5cd74127
Erin Hamalainen <erinvh620@gmail.com>
2025-04-10 09:43:47 +0100

    Added some more test code to main

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ api = MarketDataAPI(api_key=api_key, secret_key=secret_key, builder=builder)
 
 market_data_service = MarketDataService(market_data_api=api)
 
-quote = market_data_service.fetch_latest_quote("AAPL")
-print(quote)
+latets_quote_for_apple = market_data_service.fetch_latest_quote("AAPL")
+print(latets_quote_for_apple)
+
+latest_quote_for_tesla = market_data_service.fetch_latest_quote("TSLA")
+print(latest_quote_for_tesla)
+
+latest_quote_for_google = market_data_service.fetch_latest_quote("GOOG")
+print(latest_quote_for_google)
+
+latest_quote_for_alphabet = market_data_service.fetch_latest_quote("GOOGL")
+print(latest_quote_for_alphabet)
